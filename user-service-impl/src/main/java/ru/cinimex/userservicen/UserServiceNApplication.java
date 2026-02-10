8812a731a526706ac45a8c761d624c41bfdcdfe6
narasslabone <nail.galeev03@mail.ru>
2026-02-11 03:07:03 +0500

    Initial commit

---
+++ user-service-impl/src/main/java/ru/cinimex/userservicen/UserServiceNApplication.java
@@ -0,0 +1,13 @@
+package ru.cinimex.userservicen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceNApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceNApplication.class, args);
+    }
+
+}
